67c11f223b183632b965af9f653603d13b42e5da
bobista0 <borys.rybak@gmail.com>
2015-03-24 18:30:28 +0100

    Added initial project

---
+++ Grades/GradeBook.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class GradeBook
+    {
+        public GradeBook()
+        {
+            grades = new List<float>();
+        }
+
+        public GradeStatistics ComputeStatistics()
+        {
+            GradeStatistics stats = new GradeStatistics();
+            float sum = 0f;
+
+            foreach(float grade in grades)
+            {
+
+                stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
+                stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
+
+                sum = sum + grade; //sum += grade;
+            }
+
+            stats.AverageGrade = sum / grades.Count;
+
+            return stats;
+        }
+
+        public void AddGrade(float grade)
+        {
+            if (grade >= 0 && grade <= 100)
+            {
+                grades.Add(grade);
+            }
+        }
+
+        private List<float> grades;
+    }
+}
