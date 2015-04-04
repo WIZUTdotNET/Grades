1307784ee309f4858a74c52ba87498e31b6aaf1d
bobista0 <borys.rybak@gmail.com>
2015-04-04 20:39:42 +0200

    Updated code from Metody, Zdarzenia i Propercje

--- Grades/GradeBook.cs
+++ Grades/GradeBook.cs
@@ -8,8 +8,10 @@ namespace Grades
 {
     public class GradeBook
     {
-        public GradeBook()
+
+        public GradeBook(string name = "No name")
         {
+            Name = name;
             grades = new List<float>();
         }
 
@@ -18,7 +20,7 @@ namespace Grades
             GradeStatistics stats = new GradeStatistics();
             float sum = 0f;
 
-            foreach(float grade in grades)
+            foreach (float grade in grades)
             {
 
                 stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
@@ -40,7 +42,29 @@ namespace Grades
             }
         }
 
-        public string Name;
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (_name != value)
+                {
+                    var oldValue = _name;
+                    _name = value;
+                    if (NameChanged != null)
+                    {
+                        NameChangedEventArgs args = new NameChangedEventArgs();
+                        args.OldValue = oldValue;
+                        args.NewValue = value;
+                        NameChanged(this, args);
+                    }
+                }
+            }
+        }
+
+        public event NameChangedDelegate NameChanged;
+
 
         private List<float> grades;
     }
--- Grades/Program.cs
+++ Grades/Program.cs
@@ -1,61 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Markup;
 
 namespace Grades
 {
     class Program
     {
+        public delegate void Writer(string message);
+
+        static void Main(string[] args)
+        {
+            GradeBook book = new GradeBook("Borys's Book");
+            book.AddGrade(89.5f);
+            book.AddGrade(91f);
+            book.AddGrade(75f);
+
+            GradeStatistics stats = book.ComputeStatistics();
+
+            book.NameChanged += OnNameChanged;
+
+            book.Name = "Agata's book";
+
+            //WriteNames(book.Name);
+
+            Console.WriteLine(stats.AverageGrade);
+            Console.WriteLine(stats.HighestGrade);
+            Console.WriteLine(stats.LowestGrade);
+
+            book.Name = "Cezary's book";
+            book.NameChanged -= OnNameChanged;
+            book.Name = "Dorota's book";
+
+            Console.ReadLine();
+        }
+
+        private static void OnNameChanged2(object sender, NameChangedEventArgs args)
+        {
+            Console.WriteLine("***");
+        }
+
+        private static void OnNameChanged(object sender, NameChangedEventArgs args)
+        {
+            Console.WriteLine("Name changed from {0} to {1}", args.OldValue, args.NewValue);
+        }
+
         static void GiveBookAName(GradeBook book)
         {
             book = new GradeBook();
             book.Name = "Nowy dziennik";
         }
 
-        static void IncrementNumber(ref int number)
+        private static void WriteNames(params string[] names)
         {
-            number += 1;
-        }
-
-
-        static void Main(string[] args)
-        {
-
-            //Arrays();
-
-            //Immutable();
-            //PassByValueAndRef();
-
-            //GradeBook book = new GradeBook();
-            //book.AddGrade(89.5f);
-            //book.AddGrade(91f);
-            //book.AddGrade(75f);
-
-            //GradeStatistics stats = book.ComputeStatistics();
-            //Console.WriteLine(stats.AverageGrade);
-            //Console.WriteLine(stats.HighestGrade);
-            //Console.WriteLine(stats.LowestGrade);
-
-
-
-
-
-            Console.ReadLine();
-        }
-
-        private static void Arrays()
-        {
-            float[] grades;
-            grades = new float[3];
-
-            AddGrades(grades);
-
-            foreach (float grade in grades)
+            foreach (string name in names)
             {
-                Console.WriteLine(grade);
+                Console.WriteLine(name);
+            }
+        }
+
+        private static void WriteByteArray(byte[] bytes)
+        {
+            foreach (byte b in bytes)
+            {
+                Console.WriteLine("0x{0}", b);
             }
         }
 
@@ -67,31 +79,6 @@ namespace Grades
                 grades[1] = 89.1f;
                 grades[2] = 75f;
             }
-        }
-
-        private static void Immutable()
-        {
-            DateTime date = new DateTime(2002, 9, 11);
-
-            date = date.AddDays(1);
-
-            string name = " Borys ";
-            name = name.Trim();
-
-            Console.WriteLine(date);
-        }
-
-        private static void PassByValueAndRef()
-        {
-            GradeBook g1 = new GradeBook();
-            GradeBook g2 = g1;
-
-            GiveBookAName(g2);
-            Console.WriteLine(g1.Name);
-
-            int x1 = 4;
-            IncrementNumber(ref x1);
-            Console.WriteLine(x1);
         }
     }
 }
